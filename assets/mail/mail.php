0e5a2e4c42e74172fbe044dce2a972b55b9896f9
Sergey Zhukov <sergavo@gmail.com>
2018-01-20 15:25:12 +0300

    update js

--- assets/mail/mail.php
+++ assets/mail/mail.php
@@ -16,18 +16,18 @@ $outgoingServerSecurity     = 'tls';                 // Options: 'ssl' , 'tls' ,
 
 // Sending Account Settings - replace these details with an email account held on the SMTP server entered above.
 // These 2 settings are only required if you choose 'smtp' for emailMethod above.
-$sendingAccountUsername     = 'email@domain.com';
+$sendingAccountUsername     = 'zhukov.sp@gmail.com';
 $sendingAccountPassword     = 'p@55w0rd';
 
 // Recipient (To:) Details  - Change this to the email details of who will receive all the emails from the website.
-$recipientEmail             = 'email@yourdomain.com'; // REQUIRED value.
+$recipientEmail             = 'zhukov.sp@gmail.com'; // REQUIRED value.
 $recipientName              = 'John Doe';             // REQUIRED value.
 
 // Email details            - Change these to suit your website needs
 $emailSubject               = 'A message from Your Website'; // REQUIRED value. Subject of the email that the recipient will see
 $websiteName                = 'Your Website';                // REQUIRED value. This is used when a name or email is not collected from the website form
 
-$timeZone                   = 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set. 
+$timeZone                   = 'Australia/Melbourne';         // OPTIONAL, but some servers require this to be set.
                                                              // See a list of all supported timezones at: http://php.net/manual/en/timezones.php
 
 // Google reCAPTCHA
